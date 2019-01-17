5235cef07a80692c0e7725372475cdade0338d1a
wuy <yinanwu2010@hotmail.com>
2019-01-17 10:39:52 +0100

    initial message

---
+++ l1.R
@@ -0,0 +1,18 @@
+read.csv("Mash⁩ ▸ ⁨Benutzer⁩ ▸ ⁨wuy⁩ ▸ ⁨Downloads⁩/lotto_ab")
+
+data= read.csv("/Users/wuy/Downloads/LOTTO_ab_2018.csv",sep = ";",skip = 1, header=F)
+data_clean = data[,c(3:8)]
+
+
+library(tidyverse) # general utility & workflow functions
+library(tidytext) # tidy implimentation of NLP methods
+library(topicmodels) # for LDA topic modelling 
+library(tm) # general text mining functions, making document term matrixes
+library(SnowballC) # for stemming
+
+install.packages("tidyverse")
+install.packages("tidytext")
+install.packages("topicmodels")
+install.packages("tm")
+
+
